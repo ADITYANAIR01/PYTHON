ed98c018b6465e638986d0d520680e8d79b819ff
AdityaNair07 <adityanair52004@gmail.com>
2023-12-15 13:40:36 +0530

    If else  in python

---
+++ 6.If Else.py
@@ -0,0 +1,10 @@
+# If else is a block of code that will execute only  if it's condition is true
+
+age = int(input("How old are you  "))
+
+if age == 100:
+    print("You are a human ")
+elif age >=18:
+    print("YOu are an adult")
+elif age <= 18:
+    print("You are a child")
