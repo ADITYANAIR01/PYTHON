87ae282a2f442aeb4969dd829fa7b24719d0d5c9
AdityaNair07 <adityanair52004@gmail.com>
2023-12-15 12:37:42 +0530

    List in pyhthon

---
+++ Lists.py
@@ -0,0 +1,17 @@
+# List in python
+# List is used to store multiple items in a single variable
+food =["Pizza","Burger", "Momos"]
+print(food[2])  #[index of list]
+
+# We can use several functions of list by using list_name.
+food.append("Pie")    #  To add someting in list use list_name.append()
+
+food.remove("Burger") # to remove something from list
+
+# food.clear() To clear a list 
+
+
+
+
+for x in food:
+    print(x)
