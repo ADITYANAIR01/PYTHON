650ca1ca0688ff0ae8fd7da34e2e56b40c5cc864
AdityaNair07 <adityanair52004@gmail.com>
2023-12-15 12:50:21 +0530

    2D List in pyhthon

---
+++ 2.2D list.py
@@ -0,0 +1,13 @@
+# 2D List is a list of lists
+dinner = ["Pizza", "Burger", "Chicken"]   # list 1
+drinks = ["Pepsi", "Coco Cola", "Gatorade"]     # list 2
+deserts = ["Ice cream", "Cake", "Pie"]          # list 3
+
+food = [dinner,drinks,deserts] # various Lists in single variable
+
+# We can use the list. function in this too eg
+# food.remove(drinks)
+# (drinks.append("Lemonade")
+
+for x in food:
+    print(x)
