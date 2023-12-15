acde4483731d258e251f8aba7a0e2fc6b1388569
AdityaNair07 <adityanair52004@gmail.com>
2023-12-15 13:00:11 +0530

    Tuples in python

---
+++ 3. Tuples.py
@@ -0,0 +1,5 @@
+# Tuples is a collection of data which is ordered & unchangeable
+# which is used to group data together
+student1 = ("Aditya",19,"Male")
+student2 = ("Niku",17,"Female")
+print(student1.count(19)) # .count indicates the index of data i.e ad is 0, 19 is 1, Male is 2.
