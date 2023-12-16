dad2e14521802fd37c03cea3cb911107354cbe09
AdityaNair07 <adityanair52004@gmail.com>
2023-12-16 15:50:26 +0530

    +ve,-ve.0

---
+++ Q3. +ve -ve or Zero.py
@@ -0,0 +1,8 @@
+# Write a program in pyhton to check wheather a number is +ve -ve or Zero
+num1= int(input("Enter a number "))     # Declare a variable
+if(num1>0):
+    print("Number is +ve")
+elif (num1==0):
+    print("Number is zero")
+else:
+    print("Number is -ve")
