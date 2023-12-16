d8fa9684d022a764a9a3ccf8d0b432da85b1af2d
AdityaNair07 <adityanair52004@gmail.com>
2023-12-16 15:55:26 +0530

    Even Odd in python

---
+++ Q4. Even Odd.py
@@ -0,0 +1,8 @@
+# Write a program in python to find if the number is even or odd
+
+num1 = int(input("Enter a number: "))
+
+if(num1 % 2):
+    print("The number is even ")
+else:
+    print("The number is odd")
