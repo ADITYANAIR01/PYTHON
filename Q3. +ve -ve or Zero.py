aef479e580ad655c974b2e66331934992e6f96dd
AdityaNair07 <adityanair52004@gmail.com>
2023-12-16 15:51:48 +0530

    +ve,-ve.0

--- Q3. +ve -ve or Zero.py
+++ Q3. +ve -ve or Zero.py
@@ -2,7 +2,7 @@
 num1= int(input("Enter a number "))     # Declare a variable
 if(num1>0):
     print("Number is +ve")
-elif (num1==0):
+elif (num1 ==0):
     print("Number is zero")
 else:
     print("Number is -ve")
