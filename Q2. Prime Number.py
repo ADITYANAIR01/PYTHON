2fd16aa6f4f4e7f4da729b12eaff567f12856873
AdityaNair07 <adityanair52004@gmail.com>
2023-12-16 15:48:42 +0530

    Prime Numbers in pyhton

---
+++ Q2. Prime Number.py
@@ -0,0 +1,9 @@
+# Write a program in Python to find if a number is prime or not
+
+num1= int(input("Enter a number "))     # Declare a variable
+if(num1>0):
+    print("Number is +ve")
+elif (num1==0):
+    print("Number is zero")
+else:
+    print("Number is -ve")
