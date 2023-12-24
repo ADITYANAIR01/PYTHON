7f5babab9e5a06bd398c913eea9f54131c310737
Aditya Nair <121816281+ADITYANAIR01@users.noreply.github.com>
2023-12-24 16:15:18 +0530

    2d List in PYTHON

---
+++ 11. 2D list.py
@@ -0,0 +1,11 @@
+# 2D List is a list of lists
+
+drinks  = ["Coffee", "Tea", "Coca Cola"] #index 0
+dishes   = ["Pizza", "Burger", "Chicken"]  #index 1
+dessert = ["Ice cream", "Chocolate pudding", "Cake"] #index 2 
+
+# To combine multiple list use variable_name = [list1,list2,list3]
+food = [drinks, dishes, dessert]
+
+# To acess a index & item on the list use print(name[][])
+print(food[1][1])
