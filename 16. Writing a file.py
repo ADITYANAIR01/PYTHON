46e89365663c8cf9b0624aba283a575f8db7a15c
Aditya Nair <121816281+ADITYANAIR01@users.noreply.github.com>
2023-12-24 16:35:03 +0530

    Writing a file in python

---
+++ 16. Writing a file.py
@@ -0,0 +1,11 @@
+
+text =("Hi there this file is created by this code\n with open('Test File','w') as file:\n file.write(text)  ")
+# to write a file use with open('name of file','w' for write) as file:
+with open('Test File.txt','w') as file: # 'w' to write
+            file.write(text)
+
+# To append the text 
+# text = "Have a nice day, written by appending function  "
+# with open ('Test File.txt', 'a') as file:
+   # file.write(text)
+
