9f30c21d53973eaab92d539fd25998f931aca5ff
Aditya Nair <121816281+ADITYANAIR01@users.noreply.github.com>
2023-12-24 16:35:21 +0530

    Copying a file in Pyhton

---
+++ 17. Copying a file.py
@@ -0,0 +1,5 @@
+# To start copying first import shutil 
+import shutil
+
+# TO copy a file use shutil.copyfile('file to be copied','destination of the copied file')
+shutil.copyfile('14. File reading.py','copy.txt')
