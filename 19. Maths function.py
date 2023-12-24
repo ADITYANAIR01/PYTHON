f35a60df24d1720c4e5bf85d79c47dced6eb896b
Aditya Nair <121816281+ADITYANAIR01@users.noreply.github.com>
2023-12-24 16:36:45 +0530

    Maths Functions

---
+++ 19. Maths function.py
@@ -0,0 +1,20 @@
+import math
+pi = 3.14
+Num_1 = int(input(" Write a number "))
+
+# To use maths functions first import math
+
+# round function
+print(round(pi),"round")
+
+# ceilling  function i.e max round of 
+print(math.ceil(pi), "Ceil function ")
+
+# floor function     i.e min round of 
+print(math.floor(pi), "Floor function ")
+
+# power function    i.e to the power of 
+print(pow(Num_1,2),"Pow function ")
+
+# square root function
+print(math.sqrt(Num_1)," Square root")
