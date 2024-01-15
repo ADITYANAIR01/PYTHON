7bdc72fce34957f7f311f35129e0f055b3c0a2f9
AdityaNair07 <adityanair52004@gmail.com>
2024-01-15 18:34:25 +0530

    20. car for OOP

---
+++ 20. car .py
@@ -0,0 +1,13 @@
+class Car: # THE Object in OOP SHOULD BE IN CAPITAL LETTERS 
+    def __init__(self,make,model,year,colour): # to create an object we use __init__
+        self.make = make
+        self.model = model
+        self.year=year
+        self.colour
+
+    
+    def drive():
+        print("The car is driving ")
+
+    def stop():
+        print("THe car stopped driving")
