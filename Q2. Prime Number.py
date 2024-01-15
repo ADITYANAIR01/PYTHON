79b06410573220a011e3d8392e883f973a46b10f
AdityaNair07 <adityanair52004@gmail.com>
2024-01-15 19:19:59 +0530

    Prime Number

--- Q2. Prime Number.py
+++ Q2. Prime Number.py
@@ -1,2 +1,15 @@
 # Write a program in Python to find if a number is prime or not
 
+number = 12
+
+is_prime = True
+
+for i in range(2, number):
+   if number % i == 0:
+       is_prime = False
+       break
+
+if is_prime:
+   print(f"{number} is a prime number.")
+else:
+   print(f"{number} is not a prime number.")
